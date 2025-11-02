dcb0f062b90ec3deb72c595e2cee10a172d82ba3
Yaredo0 <yared4sy@gmail.com>
2025-11-02 22:13:19 +0300

    final week 2

---
+++ listings/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets
+from .models import Destination
+from .serializers import DestinationSerializer
+
+class DestinationViewSet(viewsets.ModelViewSet):
+    queryset = Destination.objects.all()
+    serializer_class = DestinationSerializer
